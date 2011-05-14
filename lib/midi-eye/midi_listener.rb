f76498354b7d3c1c508461e37933a209e94f9e3b
Ari Russo <ari.russo@gmail.com>
2011-05-14 02:54:52 -0400

    cleanup

--- lib/midi-eye/midi_listener.rb
+++ lib/midi-eye/midi_listener.rb
@@ -8,26 +8,57 @@ module MIDIEye
     end
     
     def initialize_midi_listener
-      @sources = self.class.sources.map do |hash|
+      @parser ||= Nibbler.new
+      @sources ||= []
+      @sources += self.class.sources.map do |hash|
         klass = case hash[:type]
           when :unimidi then UniMIDIInput
         end 
-        hash[:sources].each do |source|
+        [send(hash[:sources])].flatten.map do |source|
           klass.new(source)
+        end
+      end.flatten
+      listen!
+    end
+    
+    def start
+      @listener.join
+    end
+    
+    def close
+      @listener.kill
+    end
+    
+    def poll
+      events = self.class.midi_events
+      @sources.each do |input|
+        input.poll do |raw_msg|
+          unless raw_msg.nil?         
+            objs = [@parser.parse(raw_msg[:data], :timestamp => raw_msg[:timestamp])].flatten.compact
+            objs.each do |batch|
+              [batch[:messages]].flatten.each do |m|
+                hash = { :message => m, :timestamp => batch[:timestamp] }
+                events.each { |event| handle_event(event, hash) }
+              end
+            end 
+          end
         end
       end
     end
     
-    def poll
-      @sources.each do |input|
-        events = self.class.midi_events 
-        input.poll do |message|
-          events.each do |action|
-            condition = action[:condition].call(msg) unless action[:condition].nil?
-            observer.send(action[:method], msg) if condition
-          end
+    protected
+    
+    def listen!   
+      @listener = Thread.fork do       
+        while true
+          poll
         end
       end
+    end
+    
+    def handle_event(event, message)
+      condition = event[:condition].call(message) unless event[:condition].nil?
+      send(event[:method], message) if condition
     end
     
     module ClassMethods
@@ -39,9 +70,9 @@ module MIDIEye
         @midi_events << { :method => method, :condition => options[:when] }
       end
       
-      def listen_on(sources, type)
+      def listen_on(sources, options)
         @sources ||= []
-        @sources << { :sources => [sources].flatten, :type => type }
+        @sources << { :sources => sources, :type => options[:type] }
       end
       
     end
--- lib/midi-eye/unimidi_input.rb
+++ lib/midi-eye/unimidi_input.rb
@@ -5,8 +5,6 @@ module MIDIEye
     
     attr_reader :device, :pointer
     
-    Parser = Nibbler.new
-    
     def initialize(input)      
       @pointer = 0      
       @device = input      
@@ -14,14 +12,7 @@ module MIDIEye
     
     def poll(&block)
       msgs = @device.buffer.slice(@pointer, @device.buffer.length - @pointer)
-      msgs.each do |raw_msg|
-        objs = [Parser.parse(raw_msg[:data], :timestamp => raw_msg[:timestamp])].flatten.compact
-        objs.each do |batch|
-          [batch[:messages]].flatten.each do |m|
-            block.call({ :message => m, :timestamp => batch[:timestamp] })
-          end
-        end
-      end
+      msgs.each { |raw_msg| block.call(raw_msg) }
       @pointer = @device.buffer.length      
     end
     

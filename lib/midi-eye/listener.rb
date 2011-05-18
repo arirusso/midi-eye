f777b8d59e1c74715935b088ea83d4e17c5e7624
Ari Russo <ari.russo@gmail.com>
2011-05-17 20:11:27 -0400

    comments and small api changes

--- lib/midi-eye/listener.rb
+++ lib/midi-eye/listener.rb
@@ -2,6 +2,8 @@
 module MIDIEye
   
   class Listener
+    
+    attr_reader :events, :sources
       
     @input_types = []
       
@@ -13,7 +15,7 @@ module MIDIEye
       @parser = Nibbler.new
       @sources = []
       @event_queue = []
-      @midi_events = []
+      @events = []
             
       @exit_background_requested = false      
       @sources += [input].flatten.map do |i|
@@ -42,7 +44,7 @@ module MIDIEye
               [batch[:messages]].flatten.each do |single_message|
                 unless single_message.nil?
                   data = { :message => single_message, :timestamp => batch[:timestamp] }
-                  @midi_events.each { |name| queue_event(name, data) }
+                  @events.each { |name| queue_event(name, data) }
                 end
               end
             end 
@@ -51,10 +53,11 @@ module MIDIEye
       end
     end
     
-    def on_message(options = {}, &proc)
+    def listen_for(options = {}, &proc)
       return if options[:call_method].nil? && proc.nil?
-      @midi_events << { :method => options[:call_method], :proc => proc, :conditions => options }      
+      @events << { :method => options[:call_method], :proc => proc, :conditions => options }      
     end
+    alias_method :on_message, :listen_for
     
     private
         
@@ -90,9 +93,8 @@ module MIDIEye
       end
     end
     
-    def queue_event(event, message)
-      #condition = event[:condition].nil? ? false : event[:condition].call(message)  
-      @event_queue << { :action => event, :message => message } #if condition
+    def queue_event(event, message)  
+      @event_queue << { :action => event, :message => message }
     end
                 
   end  
--- lib/midi-eye/unimidi_input.rb
+++ lib/midi-eye/unimidi_input.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
+#
 module MIDIEye
   
+  # this class deals with retrieving new messages from
+  # a unimidi input buffer
   class UniMIDIInput
     
     attr_reader :device, :pointer
@@ -10,17 +13,20 @@ module MIDIEye
       @device = input      
     end
     
+    # this grabs new messages from the unimidi buffer
     def poll(&block)
       msgs = @device.buffer.slice(@pointer, @device.buffer.length - @pointer)
       @pointer = @device.buffer.length
       msgs.each { |raw_msg| yield(raw_msg) }            
     end
     
+    # if <em>input</em> is a unimidi input, this returns true
     def self.is_compatible?(input)
       klass = input.class.name.split("::").first
       klass.eql?("UniMIDI")
     end
     
+    # add this class to the Listener class' known input types
     Listener.input_types << self 
     
   end

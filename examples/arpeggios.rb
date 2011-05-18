cefa00bd8fc576f899bff6bd963fb241f9094337
Ari Russo <ari.russo@gmail.com>
2011-05-17 20:11:08 -0400

    readme and examples

--- examples/arpeggios.rb
+++ examples/arpeggios.rb
@@ -1,42 +1,56 @@
 #!/usr/bin/env ruby
-#
-# this is an example that takes plays arpeggios in time with
-# MIDI clock ticks that are received on an input
-#
-
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require "midi-eye"
 
-include MIDIEye
 include MIDIMessage
 
-input = UniMIDI::Input.first.open
-output = UniMIDI::Output.first.open
+#
+# this is an example that takes plays arpeggios in sync with
+# MIDI clock ticks that are received on an input
+#
 
-@clock = MIDIEye::Listener.new(input)
+# first, initialize the MIDI io ports
+@input = UniMIDI::Input.first.open
+@output = UniMIDI::Output.first.open
 
+# initialize the listener and give it the input port
+@clock = MIDIEye::Listener.new(@input)
+
+# the notes of the arpeggio
 @notes = [43, 46, 48, 55, 58, 61, 62, 67, 70, 72]
+
+# play a note and step through the arpeggio every time 2 notes are played 
 @ticks_per_note = 2
 
-@message_counter = 0
-@note_counter = 0
-@note_on = true
+message_counter = 0
+note_counter = 0
+note_on = true
 
-@clock.on_message(:name => "Clock") do |event|
+# look for clock messages
+@clock.listen_for(:name => "Clock") do |event|
+  
+  # is it time to output a note?
+  if message_counter.eql?(@ticks_per_note)
+    # should we send note on or note off? 
+    type = note_on ? NoteOn : NoteOff
+    # construct the note
+    note = type.new(0, @notes[note_counter], 64)
+    # output the note   
+    @output.puts(note.to_bytes)
     
-  if @message_counter.eql?(@ticks_per_note) 
-    type = @note_on ? NoteOn : NoteOff
-    note = type.new(0, @notes[@note_counter], 64)   
-    output.puts(note.to_bytes)
-    @note_on=!@note_on      
-    @note_counter = (@note_counter < (@notes.length-1) ? @note_counter + 1 : 0) if @note_on
-    @message_counter = 0
+    note_on=!note_on    
+    # step the note counter if we've finished with both note on and off for this 
+    # particular note  
+    note_counter = (note_counter < (@notes.length-1) ? note_counter + 1 : 0) if note_on
+    message_counter = 0
   else
-    @message_counter += 1
+    message_counter += 1
   end
 
 end
-  
-@clock.run
 
+p "control-c to quit..."
+
+# start the listener
+@clock.run
--- examples/transpose.rb
+++ examples/transpose.rb
@@ -1,30 +1,50 @@
 #!/usr/bin/env ruby
-#
-# this is an example that takes any note messages from a unimidi input and
-# sends them to an output transposed up one octave
-#
-#
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require 'midi-eye'
+  
+# this is an example that takes any note messages received from a unimidi input
+# and sends them to an output transposed up one octave
 
-include MIDIEye
-include MIDIMessage 
-
+# first, initialize the MIDI io ports  
 @input = UniMIDI::Input.first.open
 @output = UniMIDI::Output.first.open
 
-transpose = Listener.new(@input)
+# then create a listener for the input port  
+transpose = MIDIEye::Listener.new(@input)
 
-transpose.on_message(:class => [NoteOn, NoteOff]) do |event|
-   
+# bind an event to the listener using Listener#listen_for
+#
+# the listener will try to positively match the parameters you pass in to the properties of
+# the messages it receives
+#
+# in this example, we will look for note on/off messages
+#
+# you also have the option of leaving out the parameters altogether and including a conditional
+# in your callback (eg if event[:message].class.eql?(NoteOn) do... etc)
+#
+# you can bind as many events to a listener as you wish
+#
+transpose.listen_for(:class => [MIDIMessage::NoteOn, MIDIMessage::NoteOff]) do |event|
+
   p "from #{event[:message].note} to #{(event[:message].note + 12)}"
-  
+     
+  # raise the note value by an octave
   event[:message].note += 12
+    
+  # send the altered note message to the output you chose earlier 
   @output.puts(event[:message].to_bytes)
   
 end
 
-p "use control-c to quit..."
+# now start the listener
 
+p "control-c to quit..."
+  
 transpose.run
+  
+# or have the listener run only in a background thread by using
+# this will allow you to run multiple listeners at the same time for instance, if you're
+# trying to listen on multiple input ports
+  
+# Transpose.run(:background => true)

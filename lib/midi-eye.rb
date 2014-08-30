3a4f9c31389835c8386b70d8a76b3a878368515d
Ari Russo <ari.russo@gmail.com>
2014-08-30 13:57:26 -0400

    date, version bump

--- lib/midi-eye.rb
+++ lib/midi-eye.rb
@@ -1,20 +1,22 @@
-#!/usr/bin/env ruby
 #
 # midi-eye
-# Transparent MIDI event listener for Ruby
-# (c)2011 Ari Russo 
-# licensed under the Apache 2.0 License
+#
+# MIDI input event listener for Ruby
+# (c)2011-2014 Ari Russo 
+# Licensed under the Apache 2.0 License
 # 
 
+# libs
 require 'midi-message'
 require 'nibbler'
 require 'unimidi'
 
+# classes
 require 'midi-eye/listener'
 require 'midi-eye/unimidi_input'
 
 module MIDIEye
   
-  VERSION = "0.2.2"
+  VERSION = "0.3.1"
   
 end

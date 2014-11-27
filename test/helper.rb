9b769dd08114a68f3918a3a30fcbaa2b7b5e0d2c
Ari Russo <ari.russo@gmail.com>
2014-11-27 11:32:18 -0500

    use shoulda

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,11 @@
-#!/usr/bin/env ruby
-
 dir = File.dirname(File.expand_path(__FILE__))
-$LOAD_PATH.unshift dir + '/../lib'
+$LOAD_PATH.unshift(dir + "/../lib")
 
-require 'test/unit'
-require 'midi-eye'
+require "test/unit"
+require "mocha/test_unit"
+require "shoulda-context"
+
+require "midi-eye"
 
 module TestHelper
 

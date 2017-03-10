be4c9fce94b213e842c0f4568a7d58e8d5da0c15
Ari Russo <ari.russo@gmail.com>
2017-03-10 16:10:20 -0500

    not needed

--- lib/midi-eye/listener.rb
+++ lib/midi-eye/listener.rb
@@ -106,10 +106,8 @@ module MIDIEye
           objs.each do |batch|
             messages = [batch[:messages]].flatten.compact
             messages.each do |message|
-              unless message.nil?
-                data = { :message => message, :timestamp => batch[:timestamp] }
-                @event.enqueue_all(data)
-              end
+              data = { :message => message, :timestamp => batch[:timestamp] }
+              @event.enqueue_all(data)
             end
           end
         end
